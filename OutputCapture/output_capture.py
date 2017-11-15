27bef1a82354f45327f860338ee7f7a3b02e347e
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2017-11-14 18:10:08 -0600

    Move condense and collapse functionality to LogFormatter, added Docstrings

--- OutputCapture/output_capture.py
+++ OutputCapture/output_capture.py
@@ -41,6 +41,8 @@ TYPE_TITLE = "TITLE"
 TYPE_OTHER = "OTHER"
 LOG_TYPES = (TYPE_DEBUG, TYPE_INFO, TYPE_WARNING, TYPE_STEP, TYPE_TITLE, TYPE_OTHER)
 
+FORMAT_CONFIG_FILE_NAME = "format_config"
+
 
 class LogType(Enum):
     DEBUG = 1
@@ -104,40 +106,32 @@ class LogLine:
             self.details = input
             self.type_enum = LogType.find_log_type(input)
 
-    def condense(self, element, format_config):
-
-        collapse_dict = format_config["COLLAPSE STRUCTURES"]["dict"]
-        collapse_list = format_config["COLLAPSE STRUCTURES"]["list"]
-        condense_len = int(format_config["LENGTHS"]["condensed_elem_len"])
-        current_str = getattr(self, element)
-
-        # Collapse dicts or lists if specified
-        if collapse_dict:
-            current_str = self._collapse(current_str, "{", "}", format_config)
-        if collapse_list:
-            current_str = self._collapse(current_str, "[", "]", format_config)
-
-        # Condense length of element if it exceeds specified length
-        if len(current_str) > condense_len:
-            current_str = "".join([current_str[:condense_len - 3], "..."])
-
-        return current_str
-
-    @staticmethod
-    def _collapse(str_element, start_char, end_char, format_config):
-        start_index = str_element.find(start_char)
-        end_index = str_element.find(end_char)
-        collapse_len = int(format_config["LENGTHS"]["collapsed_struct_len"])
-        if 0 <= start_index < end_index and end_index - start_index > collapse_len:
-            return "".join([str_element[:start_index + collapse_len - 4], "...", end_char])
-        # import pdb; pdb.set_trace()
-        return str_element
 
 class LogFormatter:
+    """Class containing log line format functions"""
+
+    # Valid values from ConfigParser that result in True
     VALID_TRUE_INPUT = ("true", "yes", "t", "y", "1")
 
     @staticmethod
-    def format_line(log_line, format_config):
+    def format_line_for_console(unformatted_line, format_config):
+        """With the options within the format_config, the unformatted log line is formatted and returned.
+
+        In order for the line to be fully formatted, the log must contain the following elements and
+        order, otherwise the line will be only has the option to be condensed.
+        <Date: 9999-99-99> <Time: 99:99:99.999999> <Type> <Source> <Thread> <Details>
+
+        :param str unformatted_line: Any single line of log information
+        :param configparser.ConfigParser format_config: ConfigParser containing the options for desired
+            formatting. Refer to Create Config File function for further details.
+        :return: String of updated log line.
+        """
+
+        # Check to see if line is empty, and create log line object to parse the line
+        line = unformatted_line.strip("\n\\n")
+        if not line:
+            return ""
+        log_line = LogLine(line)
 
         # Return original line if unable to read format config file
         if not format_config.sections():
@@ -164,7 +158,7 @@ class LogFormatter:
         for elem, val in display_elements.items():
             if LogFormatter.str_to_bool(val):
                 if LogFormatter.str_to_bool(condense_elements[elem]):
-                    elements.append(log_line.condense(elem, format_config))
+                    elements.append(LogFormatter._static_condense(log_line, elem, format_config))
                 else:
                     elements.append(getattr(log_line, elem))
         output = " ".join(elements)
@@ -181,18 +175,86 @@ class LogFormatter:
             output = output[:max_len - 3] + "..."
         return output
 
+    @staticmethod
+    def _static_condense(log_line, element, format_config):
+        """Condenses each element of a log line per the format config file to a specific length, and
+        collapses lists and dictionaries that exceed a given length. All specified in the format_config
+        file. Doesn't change the values of the log_line object.
+
+        :param LogLine log_line: Line to be condensed and collapsed if requested.
+        :param str element: Name of element within the LogLine object to be condensed
+        :param configparser.ConfigParser format_config: ConfigParser containing the options for desired
+            formatting. Refer to Create Config File function for further details.
+        :returns: String of the condensed and collapsed log_line
+        """
+
+        collapse_dict = format_config["COLLAPSE STRUCTURES"]["dict"]
+        collapse_list = format_config["COLLAPSE STRUCTURES"]["list"]
+        condense_len = int(format_config["LENGTHS"]["condensed_elem_len"])
+        current_str = getattr(log_line, element)
+
+        # Collapse dicts or lists if specified
+        if collapse_dict:
+            current_str = LogFormatter._collapse(current_str, "dict", format_config)
+        if collapse_list:
+            current_str = LogFormatter._collapse(current_str, "list", format_config)
+
+        # Condense length of element if it exceeds specified length
+        if len(current_str) > condense_len:
+            current_str = "".join([current_str[:condense_len - 3], "..."])
+        return current_str
+
+    @staticmethod
+    def _collapse(str_element, data_struct, format_config):
+        """Shortens the display of the first encountered list or dictionarie to a specified length within
+        a given string.
+
+        Should update to locate recursively, but not extremely important right now.
+
+        :param str str_element: The log line element str containing the data structure
+        :param str data_struct: "list" or "dict" indicating what data structure to collapse
+        :param configparser.ConfigParser format_config: ConfigParser containing the options for desired
+            formatting. Refer to Create Config File function for further details.
+        :return: String of the element with the specified collapsed structure.
+        """
+        # Determine struct entered
+        if data_struct == "dict":
+            start_char = "{"
+            end_char = "}"
+        elif data_struct == "list":
+            start_char = "["
+            end_char = "]"
+        else:
+            return str_element
+
+        # Initialize collapse values - probably can find library to do this more efficiently
+        start_index = str_element.find(start_char)
+        end_index = str_element.find(end_char)
+        collapse_len = int(format_config["LENGTHS"]["collapsed_struct_len"])
+
+        # Collapse first data structure found - should make this to
+        if 0 <= start_index < end_index and end_index - start_index > collapse_len:
+            return "".join([str_element[:start_index + collapse_len - 4], "...", end_char])
+        return str_element
+
     @classmethod
     def str_to_bool(cls, input):
+        """Evaluates bool value of string input based on LogFormatter.VALID_TRUE_INPUT"""
         return input.lower() in cls.VALID_TRUE_INPUT
 
 
 def create_config_file():
+    """Creates a config parser file within the current working directory containing the options for
+    formatting log lines."""
+
     config = configparser.ConfigParser()
 
+    # Log lines identified by the following types to be printed or ignored
     config.add_section("DISPLAY LOG TYPES")
     config.set("DISPLAY LOG TYPES", "debug", "False")
     config.set("DISPLAY LOG TYPES", "info", "True")
 
+    # Elements within each log line to be printed or ignored
     config.add_section("DISPLAY ELEMENTS")
     config.set("DISPLAY ELEMENTS", "date", "False")
     config.set("DISPLAY ELEMENTS", "time", "True")
@@ -201,6 +263,8 @@ def create_config_file():
     config.set("DISPLAY ELEMENTS", "thread", "False")
     config.set("DISPLAY ELEMENTS", "details", "True")
 
+    # Elements within each log line to be condensed to the specified length under the LENGTHS section -
+    # condensed_elem_len
     config.add_section("CONDENSE ELEMENTS")
     config.set("CONDENSE ELEMENTS", "date", "False")
     config.set("CONDENSE ELEMENTS", "time", "False")
@@ -209,44 +273,48 @@ def create_config_file():
     config.set("CONDENSE ELEMENTS", "thread", "True")
     config.set("CONDENSE ELEMENTS", "details", "True")
 
+    # Data structures within elements to be condensed to be collapsed to the specified length under the
+    # LENGTHS section - collapsed_struct_len
     config.add_section("COLLAPSE STRUCTURES")
     config.set("COLLAPSE STRUCTURES", "list", "True")
     config.set("COLLAPSE STRUCTURES", "dict", "True")
 
+    # Various length options
     config.add_section("LENGTHS")
-    config.set("LENGTHS", "use_console_len", "True")
-    config.set("LENGTHS", "max_line_len", "200")
-    config.set("LENGTHS", "condensed_elem_len", "100")
-    config.set("LENGTHS", "collapsed_struct_len", "30")
+    config.set("LENGTHS", "use_console_len", "True")        # Use console width for max log line length
+    config.set("LENGTHS", "max_line_len", "200")            # Max length of log line to be printed
+    config.set("LENGTHS", "condensed_elem_len", "100")      # This value includes the "..."
+    config.set("LENGTHS", "collapsed_struct_len", "30")     # This value includes the "[" and "...]"
 
-    with open("format_config", "wb") as configfile:
+    # Write config to file in current working directory
+    with open(FORMAT_CONFIG_FILE_NAME, "wb") as configfile:
         config.write(configfile)
 
 
+def format_print_file_to_console(filepath):
+    """Prints a log file to the console using format information found within a format config file within
+    the current working directory. If the format config file isn't found, a new one is created using
+    default settings.
 
-def format_print_file(filepath):
+    :param str filepath: File path of the log file to be printed.
+    """
 
     # Create new format config file if it doesn't already exist in the current working directory
     local_cwd = filepath[:filepath.rfind("/") + 1]
     config_path = local_cwd + FORMAT_CONFIG_NAME
     if not os.path.isfile(config_path):
         create_config_file()
-
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    # config.read("/home/hnathani/repos/hoefer/lollygag-logger/OutputCapture/format_config")
-    with open(filepath, "r") as file:
-        for line in file.readlines():
-            if line == "\n":
-                print ""
-            else:
-                line = line.strip("\n\\n")
-                f = LogFormatter.format_line(LogLine(line), config)
-                if not f:
-                    continue
-                print f
+    # Open the log file, format each line, and print to the console.
+    with open(filepath, "r") as logfile:
+        for line in logfile.readlines():
+            f = LogFormatter.format_line_for_console(line, config)
+            print f
 
+def format_vl_output(vl_run_path):
+    pass
 
 if __name__ == '__main__':
 
@@ -261,7 +329,7 @@ if __name__ == '__main__':
         # file_path = "/home/hnathani/vl_artifacts/TsCreateDeleteAccountsVolSnap-2017-09-22T14.12.18/test.log"
         # file_path = "/home/hnathani/Documents/OutputCapture/testlogs.log"
 
-    format_print_file(file_path)
+    format_print_file_to_console(file_path)
 
 
 

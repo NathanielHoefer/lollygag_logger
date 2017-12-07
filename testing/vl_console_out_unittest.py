31c1c7f78062c46210da0ed5eca91e184de85925
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2017-12-07 16:59:09 -0600

    Move vl configparser data to separate file and include constants for sections.

--- testing/vl_console_out_unittest.py
+++ testing/vl_console_out_unittest.py
@@ -8,24 +8,71 @@ Last Updated: 12/2/2017
 
 from vl_lollygag_logger import ValenceConsoleOutput as Output
 from vl_lollygag_logger import ValenceLogLine as LogLine
-from vl_lollygag_logger import create_config_file
-from vl_lollygag_logger import FORMAT_CONFIG_FILE_NAME
+from vl_config_file import *
 import os
 import unittest
 
+from cStringIO import StringIO
+import sys
 
-class Initialization(unittest.TestCase):
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
+class InitialFormatting(unittest.TestCase):
 
     def setUp(self):
-        format_config = create_config_file()
-        self.LogFormatter = Output(LogLine, format_config)
+        self.format_config = create_config_file()
 
     def test_initialization(self):
-        self.assertTrue(self.LogFormatter.log_line_cls is LogLine)
-        self.assertTrue(len(self.LogFormatter.format_config.sections()) > 0)
+        log_formatter = Output(LogLine, self.format_config)
+        self.assertTrue(log_formatter.log_line_cls is LogLine)
+        self.assertTrue(len(log_formatter.format_config.sections()) > 0)
+
+    def test_log_newline(self):
+        log_formatter = Output(LogLine, self.format_config)
+        output = []
+        with Capturing(output) as output:
+            log_formatter.format("\n")
+            log_formatter.format("\n\n\n\\n\\n\\n\n\n\n\n\\n\\n\\n\\n\\n\\n")
+        self.assertTrue(len(output) == 1)
+        self.assertTrue(output[0] == "")
 
     def tearDown(self):
         os.remove(FORMAT_CONFIG_FILE_NAME)
+
+
+# class DisplayFormatting(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.format_config = create_config_file()
+#         self.format_config.set()
+#
+#     def test_initialization(self):
+#         log_formatter = Output(LogLine, self.format_config)
+#         self.assertTrue(log_formatter.log_line_cls is LogLine)
+#         self.assertTrue(len(log_formatter.format_config.sections()) > 0)
+#
+#     def test_log_newline(self):
+#         log_formatter = Output(LogLine, self.format_config)
+#         output = []
+#         with Capturing(output) as output:
+#             log_formatter.format("\n")
+#             log_formatter.format("\n\n\n\\n\\n\\n\n\n\n\n\\n\\n\\n\\n\\n\\n")
+#         self.assertTrue(len(output) == 1)
+#         self.assertTrue(output[0] == "")
+#
+#     def tearDown(self):
+#         os.remove(FORMAT_CONFIG_FILE_NAME)
 
 
 if __name__ == "__main__":
---
+++ vl_config_file.py
@@ -0,0 +1,77 @@
+from collections import OrderedDict
+import configparser
+
+FORMAT_CONFIG_FILE_NAME = "format_config.ini"
+
+# Section names
+DISPLAY_LOG_TYPES_SECT = "DISPLAY LOG TYPES"
+DISPLAY_FIELDS_SECT = "DISPLAY FIELDS"
+CONDENSE_FIELDS_SECT = "CONDENSE FIELDS"
+COLLAPSE_STRUCTS_SECT = "COLLAPSE STRUCTURES"
+LENGTHS_SECT = "LENGTHS"
+
+
+def create_config_file(filepath=""):
+    """Creates a config parser file within the current working directory containing the options for
+    formatting log lines.
+
+    :param str filepath: File path to store format config file. Default is current working directory.
+    """
+
+    config_fields = OrderedDict()
+
+    # Log lines identified by the following types to be printed or ignored
+    config_fields[DISPLAY_LOG_TYPES_SECT] = [
+        ("debug",   "False"),
+        ("info",    "True"),
+        ("step",    "True"),
+        ("title",   "True"),
+        ("warning", "True"),
+        ("error",   "True"),
+        ("other",   "True")]
+
+    # Elements within each log line to be printed or ignored
+    config_fields[DISPLAY_FIELDS_SECT] = [
+        ("date",    "False"),
+        ("time",    "True"),
+        ("type",    "True"),
+        ("source",  "True"),
+        ("thread",  "False"),
+        ("details", "True")]
+
+    # Elements within each log line to be condensed to the specified length under the LENGTHS
+    # section - condensed_elem_len
+    config_fields[CONDENSE_FIELDS_SECT] = [
+        ("date",    "False"),
+        ("time",    "False"),
+        ("type",    "False"),
+        ("source",  "True"),
+        ("thread",  "True"),
+        ("details", "True")]
+
+    # Data structures within elements to be condensed to be collapsed to the specified length under
+    # the LENGTHS section - collapsed_struct_len
+    config_fields[COLLAPSE_STRUCTS_SECT] = [
+        ("list", "True"),
+        ("dict", "True")]
+
+    # Various length options
+    config_fields[LENGTHS_SECT] = [
+        ("use_console_len",     "True"),    # Use console width for max log line length
+        ("max_line_len",        "200"),     # Max length of log line to be printed
+        ("condensed_field_len", "100"),     # This value includes the "..."
+        ("collapsed_struct_len", "30")]     # This value includes the "[" and "...]"
+
+    # Create and add sections and options to configparser object
+    format_config = configparser.ConfigParser()
+    for section, options in config_fields.items():
+        format_config.add_section(section)
+        for option in options:
+            format_config.set(section, option[0], option[1])
+
+    # Write config to file in current working directory
+    filepath = filepath + FORMAT_CONFIG_FILE_NAME if filepath else FORMAT_CONFIG_FILE_NAME
+    with open(filepath, "wb") as configfile:
+        format_config.write(configfile)
+
+    return format_config

9e2ff56fd71b0b19657f0451e8b3d60c137418f1
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2017-12-14 18:34:42 -0600

    Add LollygagLogger unittest to test file and process stream

---
+++ testing/test_program_stream.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+=========================================================================================================
+Test Program Stream
+=========================================================================================================
+by Nathaniel Hoefer
+Last Updated: 12/14/2017
+
+Used solely for testing LollygagLogger by printing values a second apart.
+"""
+
+from time import sleep
+
+if __name__ == "__main__":
+    for i in range(3):
+        print i
+        sleep(1)

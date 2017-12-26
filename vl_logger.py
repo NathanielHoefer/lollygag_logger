9fdddb8df3665469b7e94a17850828eabbe2afa1
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2017-12-26 16:58:46 -0600

    Update args in vl_logger

--- vl_logger.py
+++ vl_logger.py
@@ -48,43 +48,51 @@ from requests import auth
 
 from bin.lollygag_logger import LollygagLogger
 from bin.vl_console_module.vl_config_file import *
-from bin.vl_console_module import ValenceConsoleFormatter
-from bin.vl_console_module import ValenceLogLine
+from bin.vl_console_module import ValenceConsoleFormatter as Formatter
+from bin.vl_console_module import ValenceLogLine as LogLine
 
-# Descriptions for arg parse
-PROGRAM = "Lollygag Logger"
-DESCRIPTION = "This script will print out formatted logs from a log file or format the output of the " \
-              "vl run command."
-ARG_VL_DESC = "By default, the path to suite as in the vl_run command. Also used by -read and -at2 flags"
-ARG_FILE_DESC = "Read from log file in vl_path."
-ARG_AT2_DESC = "Fetch AT2 Task Instance logs from taskID in vl_path"
-ARG_FIND_DESC = "Highlight specified string found in the logs."
-ARG_LIST_DESC = "List specified test case or step. For test case, match 'Test Case #'. If" \
-                "specifying step, list full name out as seen in logs."
-ARG_SAVE_DESC = "Save log output to specified file."
+
+def args():
+    # Descriptions for arg parse
+    program = "Valence Lollygag Logger"
+    description = "Formats Valence Logs based on custom user preferences to assist in debugging. This " \
+                  "tool can format logs from the 'vl run' command, stored log file, or an at2 task " \
+                  "step instance. When formatting logs from the 'vl run' command, the tool will " \
+                  "execute the command directly and the output will be in real time."
+    vl_desc = "Source of the logs. Without -r or -at2 flags, this tool will attempt to execute the " \
+              "'vl run' command using the vl_source parameter as the suite path. To cancel the " \
+              "'vl run' execution, pass a keyboard interrupt and the test will be cancelled. Refer " \
+              "to -r or -at2 descriptions for additional info."
+    file_desc = "Flag indicating to read from log file specified in the vl_source parameter."
+    at2_desc = "Flag indicating to fetch AT2 Task Instance logs from the specified AT2 Task Step " \
+               "Instance ID specified in the vl_source parameter."
+    find_desc = "Highlight specified string found in the logs."
+    list_desc = "List specified test case or step. For test case, match 'Test Case #'. If specifying " \
+                "step, list full name out as seen in logs."
+    write_desc = "Write log output to specified file."
+
+    # Argument setup and parsing
+    parser = argparse.ArgumentParser(prog=program, description=description)
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument("vl_source", help=vl_desc)
+    group.add_argument("-r", "--read", action="store_true", help=file_desc)
+    group.add_argument("-at2", action="store_true", help=at2_desc)
+    parser.add_argument("-f", "--find", action="store", dest="find_str", help=find_desc)
+    parser.add_argument("-l", "--list", action="store", dest="list_step", help=list_desc)
+    parser.add_argument("-w", "--write", action="store", dest="write_path", help=write_desc)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-
-    # Argument setup and parsing
-    parser = argparse.ArgumentParser(prog=PROGRAM, description=DESCRIPTION)
-    group = parser.add_mutually_exclusive_group()
-    parser.add_argument("vl_path", help=ARG_VL_DESC)
-    group.add_argument("-r", "--read", action="store_true", help=ARG_FILE_DESC)
-    group.add_argument("-at2", action="store_true", help=ARG_AT2_DESC)
-    parser.add_argument("-f", "--find", action="store", dest="find_str", help=ARG_FIND_DESC)
-    parser.add_argument("-l", "--list", action="store", dest="list_step", help=ARG_LIST_DESC)
-    parser.add_argument("-s", "--save", action="store", dest="save_path", help=ARG_SAVE_DESC)
-    args = parser.parse_args()
+    args = args()
 
     # Validate that args exist and execute printing the logs
-    if args.vl_path:
+    if args.vl_source:
         config = create_config_file()
-        vl_console_output = ValenceConsoleFormatter(ValenceLogLine, config,
-                                                    args.find_str, args.list_step, args.save_path)
+        vl_console_output = Formatter(LogLine, config, args.find_str, args.list_step, args.write_path)
         if not args.read and not args.at2:
             # Begin vl run subprocess
-            proc = subprocess.Popen(["vl", "run", args.vl_path], stdout=subprocess.PIPE,
+            proc = subprocess.Popen(["vl", "run", args.vl_source], stdout=subprocess.PIPE,
                                     bufsize=1, universal_newlines=False)
             try:
                 logger = LollygagLogger(iter(proc.stdout.readline, b''), vl_console_output)
@@ -95,7 +103,7 @@ if __name__ == '__main__':
                 proc.kill()
         elif args.read:
             try:
-                arg_path = args.vl_path
+                arg_path = args.vl_source
                 if arg_path[0] == "/" or arg_path[0] == "~":
                     file_path = arg_path
                 else:
@@ -116,7 +124,7 @@ if __name__ == '__main__':
                       "the {0} file.".format(FORMAT_CONFIG_FILE_NAME)
                 exit(0)
 
-            STEP_ID = args.vl_path
+            STEP_ID = args.vl_source
             AT2_STREAM_URL = 'https://autotest2.solidfire.net/stream/stdout/{}/'.format(STEP_ID)
             AUTH = auth.HTTPBasicAuth(AT2_USER, AT2_PASS)
             session = requests.Session()
@@ -132,6 +140,6 @@ if __name__ == '__main__':
                 raise
             finally:
                 resp.close()
-            print "AT2 option selected. TaskID: {0}.".format(args.vl_path)
+            print "AT2 option selected. TaskID: {0}.".format(args.vl_source)
         else:
             print "Please pass valid arguments"

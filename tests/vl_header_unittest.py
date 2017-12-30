fdb650a9d3315b352cc1fe7fc4582281aa014159
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2017-12-30 13:35:31 -0600

    Header operations moved to format function

--- tests/vl_header_unittest.py
+++ tests/vl_header_unittest.py
@@ -23,7 +23,7 @@ class HeaderParse(unittest.TestCase):
     def test_case_title(self):
         original_line = "Test Case 1: Starting Teardown of TcGenericTestCase"
         header = ValenceHeader(original_line)
-        self.assertEqual(header.type, HeaderType.TEST_CASE)
+        self.assertEqual(header.header_type, HeaderType.TEST_CASE)
         self.assertEqual(header.test_name, "TcGenericTestCase")
         self.assertEqual(header.test_info, "Test Case 1")
         self.assertEqual(header.test_instruction, "Starting Teardown of TcGenericTestCase")
@@ -32,7 +32,7 @@ class HeaderParse(unittest.TestCase):
     def test_suite_title(self):
         original_line = "Test Suite: Starting Teardown of TsGenericTestSuite"
         header = ValenceHeader(original_line)
-        self.assertEqual(header.type, HeaderType.SUITE)
+        self.assertEqual(header.header_type, HeaderType.SUITE)
         self.assertEqual(header.test_name, "TsGenericTestSuite")
         self.assertEqual(header.test_info, "Test Suite")
         self.assertEqual(header.test_instruction,
@@ -42,7 +42,7 @@ class HeaderParse(unittest.TestCase):
     def test_other_title(self):
         original_line = "All Test Case Preconditions"
         header = ValenceHeader(original_line)
-        self.assertEqual(header.type, HeaderType.VALENCE)
+        self.assertEqual(header.header_type, HeaderType.VALENCE)
         self.assertEqual(header.test_name, "")
         self.assertEqual(header.test_info, "")
         self.assertEqual(header.test_instruction, "")
@@ -51,7 +51,7 @@ class HeaderParse(unittest.TestCase):
     def test_case_step(self):
         original_line = "Starting Step 2 for TcGenericTestCase: Completed Monitoring"
         header = ValenceHeader(original_line)
-        self.assertEqual(header.type, HeaderType.STEP)
+        self.assertEqual(header.header_type, HeaderType.STEP)
         self.assertEqual(header.test_name, "TcGenericTestCase")
         self.assertEqual(header.test_info, "Starting Step 2 for TcGenericTestCase")
         self.assertEqual(header.test_instruction, "Completed Monitoring")

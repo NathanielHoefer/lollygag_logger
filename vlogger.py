1a58761eccde1edf2b6fe6bde085c7e06cf99fb3
Nathaniel <nathaniel.d.hoefer@gmail.com>
2018-08-07 13:55:14 -0500

    Mod: vlogger to run suite

--- vlogger.py
+++ vlogger.py
@@ -7,16 +7,16 @@ Formats the logs to be output to the screen.
 import argparse
 import os
 import re
-import subprocess
-
-from bin.vconfiginterface import VConfigInterface
-from bin.vformatter import VFormatter
 
 from bin.lollygag_logger import LollygagLogger
+from bin.vconfiginterface import VConfigInterface
+from bin.vformatter import VFormatter
+from plugins import at2_task
+from plugins import vl_run
 
 FILE_PATTERN = "^(?:\w|-|/|\.)+\.log$"
 AT2_PATTERN = "^\d+$"
-SUITE_PATTERN = "^Ts(?:\w|-)+$"
+SUITE_PATTERN = "^(?:\w|-|/|\.)*Ts(?:\w|-)+$"
 
 
 def args():
@@ -32,130 +32,80 @@ def args():
                   "tool can format logs from the 'vl run' command, stored log file, or an at2 task " \
                   "step instance. When formatting logs from the 'vl run' command, the tool will " \
                   "execute the command directly and the output will be in real time."
-    read_desc = "Read from log file specified."
-    at2_desc = ""  # TODO
     log_source = "Log File (*.log) | AT2 Task Inst. Step ID | Suite Path"
     testcase_desc = "(tc_name|tc_number)[:step number] - List specified test case and optionally step"
     format_api_desc = "Display API calls"
     save_desc = "Filepath - Store formatted logs to a file."  # TODO
-    file_desc = ""  # TODO
 
     # Argument setup and parsing
     parser = argparse.ArgumentParser(prog=program, description=description)
-    # group = parser.add_mutually_exclusive_group()
     parser.add_argument("log_source", nargs=1, help=log_source)
-    # group.add_argument("-r", "--read", action="store", dest="read_path", help=read_desc)
-    # group.add_argument("-at2", action="store", dest="at2_inst", help=at2_desc)
     parser.add_argument("-t", "--testcase", action="store", dest="testcase", help=testcase_desc)
     parser.add_argument("-a", "--api", action="store_true", dest="format_api", help=format_api_desc)
     parser.add_argument("-s", "--save", action="store_true", dest="save", help=save_desc)
     return parser.parse_args()
 
+
 if __name__ == '__main__':
 
+    # Variable Init ***********************************************************
+
+    args = args()
 
     logger = None
+    log_source = args.log_source[0]
+    logfile = ""
 
-    COMMAND_LINE = True
+    savedfile = re.match(FILE_PATTERN, log_source)
+    at2_instance = re.match(AT2_PATTERN, log_source)
+    suite = re.match(SUITE_PATTERN, log_source)
 
-    if COMMAND_LINE:
+    # Log source **************************************************************
+    # - Handle any log source specific operations
 
-        # Variable Init *******************************************************
+    if savedfile:
+        logfile = log_source
 
-        args = args()
+    elif at2_instance:
+        logfile = at2_task.logs_from_at2(log_source)
 
-        log_source = args.log_source[0]
+    elif suite:
+        logfile = vl_run.execute_local_run(log_source)
 
-        savedfile = re.match(FILE_PATTERN, log_source)
-        at2_instance = re.match(AT2_PATTERN, log_source)
-        suite = re.match(SUITE_PATTERN, log_source)
+    else:
+        print "Invalid log source."
+        exit(1)
 
-        logfile = ""
-        save_filepath = ""
+    # Test Cases **************************************************************
 
-        # Log source **********************************************************
-        # - Handle any log source specific operations
-
-        if savedfile:
-            logfile = log_source
-            save_filename = "fmt_%s" % os.path.basename(logfile)
-            save_filepath = os.path.join(os.path.dirname(logfile), save_filename)
-
-        elif at2_instance:
-            tmp_config = VConfigInterface()
-            AT2_USER, AT2_PASS, FETCH_PATH = tmp_config.get_at2_info()
-
-            at2_log_file = "%s.log" % log_source
-            _, cwd = os.path.split(os.getcwd())
-            if cwd == log_source:
-                save_dir = os.getcwd()
+    # Display specific test cases and steps
+    if (savedfile or at2_instance) and args.testcase:
+        tmp_config = VConfigInterface()
+        tmp_config.use_unformatted()
+        tmp_formatter = VFormatter(tmp_config)
+        m = re.match("^(\d+|Tc\w*)(:(\d+))*$", args.testcase)
+        # Test case specified
+        if m.group(1):
+            if m.group(1).isdigit():
+                logfile = tmp_formatter.parse_test_case(logfile, tc_num=int(m.group(1)))
             else:
-                save_dir = os.path.join(tmp_config.get_save_dir(), log_source)
-                if not os.path.exists(save_dir):
-                    os.mkdir(save_dir)
-            logfile = os.path.join(save_dir, at2_log_file)
+                logfile = tmp_formatter.parse_test_case(logfile, tc_name=m.group(1))
+        # Step specified
+        if m.group(3) and m.group(3).isdigit():
+            logfile = tmp_formatter.parse_step(logfile, step_num=int(m.group(3)))
 
-            print "AT2 option selected. TaskID: {0}.".format(log_source)
-            if not os.path.exists(logfile):
-                print "Downloading from AT2..."
-                command = []
-                command.extend(["python", "%s" % FETCH_PATH, "%s" % log_source])
-                command.extend(["-o", "%s" % logfile])
-                command.extend(["-u", "%s" % AT2_USER])
-                command.extend(["-p", "%s" % AT2_PASS])
+    # Additional Configuration ************************************************
 
-                try:
-                    subprocess.check_call(command)
-                except:
-                    if not os.path.exists(logfile):
-                        print "Unable to fetch AT2 logs. Check fetch-task-instance-script-path in .ini file."
-                        exit(1)
+    config = VConfigInterface()
 
-                    print "Unable to fetch AT2 logs. Check AT2 credentials in .ini file."
-                    os.remove(logfile)
-                    exit(1)
-
-
-            is_at2 = True
-            save_filepath = os.path.join(save_dir, "fmt_%s" % at2_log_file)
-
-        elif suite:
-            pass
-        else:
-            print "Invalid log source."
-            exit(1)
-
-        # Test Cases **********************************************************
-
-        # Display specific test cases and steps
-        if (savedfile or at2_instance) and args.testcase:
-            tmp_config = VConfigInterface()
-            tmp_config.use_unformatted()
-            tmp_formatter = VFormatter(tmp_config)
-            m = re.match("^(\d+|Tc\w*)(:(\d+))*$", args.testcase)
-            # Test case specified
-            if m.group(1):
-                if m.group(1).isdigit():
-                    logfile = tmp_formatter.parse_test_case(logfile, tc_num=int(m.group(1)))
-                else:
-                    logfile = tmp_formatter.parse_test_case(logfile, tc_name=m.group(1))
-            # Step specified
-            if m.group(3) and m.group(3).isdigit():
-                logfile = tmp_formatter.parse_step(logfile, step_num=int(m.group(3)))
-
-            save_filename = "fmt_%s" % os.path.basename(logfile)
-            save_filepath = os.path.join(os.path.dirname(logfile), save_filename)
-
-
-        # Additional Configuration ********************************************
-
-        config = VConfigInterface()
-
+    if savedfile or at2_instance:
         is_at2 = config.is_at2_formatting(logfile)
         if is_at2:
             config.at2_format()
 
         if args.save:
+            save_filename = "fmt_%s" % os.path.basename(logfile)
+            save_filepath = os.path.join(os.path.dirname(logfile), save_filename)
             print "Saving formatted logs to %s..." % save_filepath
             open(save_filepath, 'w').close()
             word_count = sum(1 for line in open(logfile))
@@ -164,39 +114,18 @@ if __name__ == '__main__':
         if args.format_api:
             config.format_api()
 
-        # Execute vlogger *****************************************************
+    # Execute vlogger *********************************************************
 
-        vl_console_output = VFormatter(config)
-        try:
+    vl_console_output = VFormatter(config)
+    try:
+        if suite:
+            logger = LollygagLogger(iter(logfile.stdout.readline, b''), vl_console_output)
+            logger.run()
+        else:
             with open(logfile, "r") as logfile:
                 logger = LollygagLogger(logfile, vl_console_output)
                 logger.run()
-        except KeyboardInterrupt:
-            logger.kill()
-            print "Keyboard Interrupt: Exiting Logger"
-            exit(0)
-
-    else:
-        # path = "/home/nathaniel/vl_artifacts/TsDriveEncryptionPersistenceAndAccessibility-2018-02-07T16.14.18/test.log"
-        path = "/home/hnathani/vl_artifacts/285465966/285465966_format.log"
-
-
-        config = VConfigInterface()
-        config.at2_format()
-
-        config.display_test_case(number=5)
-        # config.display_step(number=1)
-        config.display_summary(True)
-
-        # config.use_unformatted()
-
-        vl_console_output = VFormatter(config)
-        # vl_console_output.parse_test_case(path, tc_num=0)
-        try:
-            with open(path, "r") as logfile:
-                logger = LollygagLogger(logfile, vl_console_output)
-                logger.run()
-        except KeyboardInterrupt:
-            logger.kill()
-            print "Keyboard Interrupt: Exiting Logger"
-            exit(0)
+    except KeyboardInterrupt:
+        logger.kill()
+        print "Keyboard Interrupt: Exiting Logger"
+        exit(0)
